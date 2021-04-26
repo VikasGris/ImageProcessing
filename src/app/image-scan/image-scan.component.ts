ae0a5eb346e32746d20835aaa906001f8d85f818
VikasGris <vikasgowri0913@gmail.com>
2021-04-26 09:33:16 +0530

    Added Image scanner

--- src/app/image-scan/image-scan.component.ts
+++ src/app/image-scan/image-scan.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import Dynamsoft from 'dwt';
+import { WebTwain } from 'dwt/dist/types/WebTwain';
 
 import { ResponseService } from '../response.service';
 
@@ -21,14 +23,20 @@ export class ImageScanComponent implements OnInit {
   form;
   error: any = null;
   isLoading = false;
-  hasFinishedReading = false;
   selectDropdownId;
   getResult = false;
-  idSelected = false;
   finalresult;
+  success;
   successAlert:boolean = false;
   errorAlert:boolean = false;
+  deleteImage;
 
+  //Dynamic Web Twin:
+  DWObject:WebTwain;
+  selectSources:HTMLSelectElement;
+  containerId = 'card1';
+  bwASM = Dynamsoft.Lib.env.bMobile || !Dynamsoft.DWT.UseLocalService
+  
   
 
   constructor(private service: ResponseService) {
@@ -36,57 +44,107 @@ export class ImageScanComponent implements OnInit {
 
   ngOnInit(): void {
     this.form = new FormGroup({
-      files: new FormControl(''),
-      select: new FormControl("", Validators.required)
+      files: new FormControl(null,Validators.required),
+      select: new FormControl(null, Validators.required)
     });
+    Dynamsoft.DWT.Containers = [{ WebTwainId: 'dwtObject', ContainerId: this.containerId, Width:'auto', Height:'auto' }];
+    Dynamsoft.DWT.RegisterEvent('OnWebTwainReady', () => { this.Dynamsoft_OnReady(); });
+    console.log(Dynamsoft.DWT)
+    Dynamsoft.DWT.ProductKey = 't00891wAAAJyHArHjdRL0wBNHC47fVCY41/FatXNtYRsY6D/2tMOnqU3ecIoRTzEw1WNKa7lZJEgzA3fD39lzbscdtF5Wtxa/Cwnz3QLUgU8QaQCj65BTN2rtK7Q=';
+    Dynamsoft.DWT.ResourcesPath = 'assets/Resources';
+    Dynamsoft.DWT.Load();
   }
+
+  //DWT
+
+  Dynamsoft_OnReady(): void {
+    this.DWObject = Dynamsoft.DWT.GetWebTwain('card1');
+    let count = this.DWObject.SourceCount;
+    console.log(count)
+    this.selectSources = <HTMLSelectElement>document.getElementById("sources");
+    this.selectSources.options.length = 0;
+    for (let i = 0; i < count; i++) {
+      this.selectSources.options.add(new Option(this.DWObject.GetSourceNameItems(i), i.toString()));
+    }
+  }
+
+  acquireImage(): void{
+    if(!this.DWObject){
+      this.DWObject = Dynamsoft.DWT.GetWebTwain('card1');
+      console.log(Dynamsoft);
+      console.log("First If",this.DWObject)
+    }
+    if(this.DWObject.SourceCount > 0 && this.DWObject.SelectSourceByIndex(this.selectSources.selectedIndex)){
+      const onAcquireImageSuccess = () => { this.DWObject.CloseSource();};
+      const onAcquireImageFailure = onAcquireImageSuccess;
+      this.DWObject.OpenSource();
+      this.DWObject.AcquireImage( {}, onAcquireImageSuccess,onAcquireImageFailure);
+      console.log(this.DWObject)
+    }
+    
+    else{
+      alert("No Source Available!");
+      console.log("No source")
+    }
+  }
+
+  
+
 
   onFileChange(event: any): void {
     var inputFile = event.target.files[0];
+    console.log(event.target.files)
     if (inputFile) {
       const reader = new FileReader();
       reader.onload = this.handleReaderLoaded.bind(this);
       reader.readAsBinaryString(inputFile)
-      this.hasFinishedReading = true;
-      console.log(this.hasFinishedReading)
-      
     }
   }
 
   handleReaderLoaded(e) {
     this.base64textString.push('data:image/png;base64,' + btoa(e.target.result));
-    //console.log(this.base64textString)
+    console.log(this.base64textString)
   }
+
+  // onDelete(){
+  //   this.deleteImage = this.base64textString
+  //   console.log(this.deleteImage)
+  //   this.deleteImage.shift()
+  //   console.log(this.deleteImage)
+  // }
+
+  // onRescan(e){
+  //   console.log(this.deleteImage)
+  //   console.log(this.base64textString)
+  //   this.base64textString.splice(0,0,this.onFileChange)
+  // }
 
   selectId(event) {
      //console.log(event.target.value);
      this.selectDropdownId = this.form.value.select;
-    this.idSelected = true
-    console.log(this.idSelected)
   }
   get f() {
     return this.form.controls;
   }
 
+  onReset(){
+    location.reload();
+  }
 
   onSubmit() {
+    this.isLoading = true;
     const params = {
       '_id': this.selectDropdownId,
       'page': this.base64textString,
     };
-    
-    //console.log('temp', params);
-    //console.log(this.form.value)
     this.service.postResponse(params).subscribe(response => {
       this.result = response
       this.error = null
-      this.isLoading = true;
+      this.isLoading = false;
       this.getResult = true;
     }, (error) => {
       this.error = error.statusText;
       this.isLoading = false
-      
-      
     })
   }
 
@@ -125,20 +183,20 @@ export class ImageScanComponent implements OnInit {
       }
     };
     this.service.postResponseSave(finalOutput).subscribe(response => {
-      if(response.code === "success"){
-        location.reload()
-           this.successAlert = true;
-        // this.base64textString = [];
-        // this.result = null;
-        // this.form.value.select = null;
-           this.form.reset();
-        // console.log(this.form.reset())
-      }
-      else{
-        this.errorAlert = true;
-      }
+      this.success = response;
+      location.reload();
+      // if(response.code === "success"){
+      //   location.reload()
+      //      this.successAlert = true;
+      //   this.base64textString = [];
+      //   this.result = null;
+      //   this.form.value.select = null;
+      //      this.form.reset();
+      //   console.log(this.form.reset())
+      // }
       console.log("Final Response", response)
     }, (error) => {
+      this.errorAlert = true;
       console.log("Final Error", error);
     })
   }
