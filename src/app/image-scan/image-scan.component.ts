e93cc46532b88742c2694a7e162ac3d706ae3db5
VikasGris <vikasgowri0913@gmail.com>
2021-06-03 11:00:56 +0530

    Working on fixing bugs

--- src/app/image-scan/image-scan.component.ts
+++ src/app/image-scan/image-scan.component.ts
@@ -16,53 +16,51 @@ export class ImageScanComponent implements OnInit {
     
 //Variables declarations
   base64textString = [];
-  listOfDocuments: any = ["Nalan Gastro Centre", "Nalan Diagnostics" , "Pathway Diagnostics","Bioline"];
+  fileNames = [];
+  listOfDocuments:string[] = ["Nalan Gastro Centre", "Nalan Diagnostics" , "Pathway Diagnostics","Bioline"];
   result = {};
   result__keys =[];
   result_testreport={};
-  result_imp=[];
-  result_date=null;
+  result_imp:any[]=[];
+  result_date:any[]=[];
   result_others = {};
+  showFileName: boolean = false;
   result_date_show:boolean = false;
   result_imp_show:boolean = false;
   result_testreport_show:boolean = false;
+  Select_document_disabled: boolean = false;
   upArrow:boolean = false;
   downArrow:boolean = false;
-  closeIcon = true;
-  disabledupload=true;
+  closeIcon:boolean = true;
+  disabledupload:boolean=true;
   image_view='data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==';
-  form;
+  form:FormGroup;
   error: boolean = false;
-  isLoading = false;
-  selectDropdownId;
-  getResult = false;
+  isLoading:boolean = false;
+  selectDropdownId:string="";
+  getResult:boolean = false;
   success={code:null,
-  message:null};
-  uploadFileAlert = false;
-  DropdownId = false;
+          message:null};
+  uploadFileAlert:boolean = false;
+  DropdownId:boolean = false;
   successAlert:boolean = false;
   errorAlert:boolean = false;
   zoom:boolean = false;
   largeImage:boolean = true;
   zoomIcon:boolean = false;
-  successCount = 0;
-  failedCount = 0;
+  successCount: number = 0;
+  failedCount: number = 0;
   uploadButton:boolean = false;
   responseButton:boolean = false;
-  averageTime = 10;
-  seconds = 1;
-  averageTime_Index = 0;
-  avgSeconds
+  averageTime: number = 10;
+  seconds: number = 1;
+  averageTime_Index: number = 0;
+  avgSeconds: number;
   newDocumentInput = false;
   duplicate_browse = false;
-  BrowseId='';
-  markForReview = false;
-  closeResult;
-  DocumentIdUploaded = '';
-  reportData;
-  rdta;
-  test_Report;
-  input_Id;
+  BrowseId:string='';
+  markForReview:boolean = false;
+  DocumentIdUploaded:string = '';
   waitForResponse: boolean = false;
   verifyDocumentId:boolean = false;
   invalidDocumentId:boolean = false;
@@ -116,22 +114,34 @@ export class ImageScanComponent implements OnInit {
     this.closeIcon = true;
     this.uploadFileAlert = false;
     var inputFile = event.target.files;
-    console.log(inputFile);
+    for (var i = 0; i < inputFile.length; i++){
+      this.fileNames.push(inputFile[i].name);
+    }
     for (var i = 0; i < inputFile.length; i++){
        if (inputFile[i]) {
         const reader = new FileReader();
         reader.onload = this.handleReaderLoaded.bind(this);
         reader.readAsBinaryString(inputFile[i])
-      }
+      } 
     }
+    if (this.Select_document_disabled === true) {
+      this.disabledupload = false;
+    }
+    
   }
 
+  fileNameShow() {
+    this.showFileName = true;
+  }
+  fileNameNotShow() {
+    this.showFileName = false;
+  }
 //Store files in array
   handleReaderLoaded(e) {
     this.base64textString.push('data:image/png;base64,' + btoa(e.target.result));
     this.image_view = this.base64textString[this.base64textString.length-1];
     this.zoomIcon = true;
-    console.log(this.base64textString)
+    //console.log(this.base64textString)
   }
   
 
@@ -154,33 +164,11 @@ export class ImageScanComponent implements OnInit {
     //console.log(this.image_view)
   }
 
-//Functionality for edit option given to displaying result
-  onUpdate(){
-    //console.log(this.formTable.get('scanCenterName').value);
-  }
-
-  openEditableInput1(){
-    this.formTable.get('scanCenterName').enable();
-  }
-  openEditableInput2(){
-    this.formTable.get('patientName').enable();
-  }
-  openEditableInput3(){
-    this.formTable.get('reportType').enable();
-  }
-  
-  openEditableInput4(){
-    this.formTable.get('reportDate').enable();
-  }
-  openEditableInput5(){
-    this.formTable.get('impression').enable();
-  }
-
-  
 //Remove  file from the list
   
   removeSelectedFile(index) {
-    this.base64textString.splice(index,1);
+    this.base64textString.splice(index, 1);
+    this.fileNames.splice(index, 1);
     this.image_view = this.base64textString[this.base64textString.length - 1];
     if(this.base64textString.length === 0){
       this.zoomIcon = false;
@@ -196,6 +184,7 @@ export class ImageScanComponent implements OnInit {
     this.zoom = true;
     if(this.largeImage===true){
       this.largeImage = false;
+      this.BrowseId='';
     }
     else if (this.duplicate_browse===true) {
       this.duplicate_browse=false;
@@ -220,33 +209,25 @@ export class ImageScanComponent implements OnInit {
   selectId(event) {
     this.newDocumentInput = false;
     this.DropdownId = false;
-    if (this.form.value.select === 'Nalan Gastro Centre') {
-      
-    }
     if (this.form.value.select !== 'Select Document') {
+      this.Select_document_disabled = true;
+      this.selectDropdownId = this.form.value.select;
       if (this.base64textString.length !== 0) {
-        this.selectDropdownId = this.form.value.select;
+        
         this.disabledupload = false;
-      
-        if (this.selectDropdownId === "New_Document") {
-          this.newDocumentInput = true;
-          this.disabledupload = true;
-          this.markForReview = true;
-        }
-        else {
-          this.markForReview = false;
+        this.markForReview = false;
 
-          if (this.DocumentIdUploaded !== '') {
-            if (this.DocumentIdUploaded !== this.form.value.select) {
-              this.markForReview = false;
-              this.getResult = false;
-            }
-            else {
-            
-              this.markForReview = true;
-              this.getResult = true;
-            }
+        if (this.DocumentIdUploaded !== '') {
+          if (this.DocumentIdUploaded !== this.form.value.select) {
+            this.markForReview = false;
+            this.getResult = false;
           }
+          else {
+          
+            this.markForReview = true;
+            this.getResult = true;
+          }
+          
         }
       }
     }
@@ -271,7 +252,9 @@ export class ImageScanComponent implements OnInit {
 //Reset form fields and some other fields
 
   onReset() {
+
     this.closebutton2.nativeElement.click();
+    this.fileNames = [];
     this.error = false;
     this.uploadFileAlert = false;
     this.waitForResponse = false;
@@ -283,18 +266,18 @@ export class ImageScanComponent implements OnInit {
     this.largeImage = true;
     this.upArrow = false;
     this.downArrow = false;
-    this.form.reset();
-    this.form.value.select = null;
+    //this.form.value.select = null;
     this.image_view = "data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==";
     this.zoomIcon = false;
     this.newDocumentInput = false;
     this.markForReview = false;
     this.getResult = false;
     this.base64textString = [];
+    
     this.result={};
     this.result_testreport={};
     this.result_imp=[];
-    this.result_date=null;
+    this.result_date=[];
     this.result_others = {};
     this.result_date_show = false;
     this.result_imp_show = false;
@@ -305,12 +288,17 @@ export class ImageScanComponent implements OnInit {
     this.disabledupload = true; 
     this.tableShow = false;
     this.DocumentIdUploaded = "";
+    // this.form.reset();
+    this.Select_document_disabled = false;
+    this.selectDropdownId = "";
+    
   }
 
   onEdit() {
+    this.Select_document_disabled = false;
+    this.selectDropdownId = "";
     this.closebutton2.nativeElement.click();
-    this.form.reset();
-    this.form.value.select = null;
+    //this.form.value.select = null;
     this.error = false;
     this.closeIcon = true;
     this.largeImage = true;
@@ -328,7 +316,7 @@ export class ImageScanComponent implements OnInit {
     this.result={};
     this.result_testreport={};
     this.result_imp=[];
-    this.result_date=null;
+    this.result_date=[];
     this.result_others = {};
     this.result_date_show = false;
     this.result_imp_show = false;
@@ -340,7 +328,7 @@ export class ImageScanComponent implements OnInit {
     this.tableShow = false;
     this.DocumentIdUploaded = "";
     if (this.tableShow) {
-      console.log(this.tableShow)
+      //console.log(this.tableShow)
       this.getResult = false;
       this.markForReview = false;
     }
@@ -367,8 +355,11 @@ export class ImageScanComponent implements OnInit {
   }
   onchangeInput_0(event) {
     var key = event.target.key;
-    var _id = event.target.row;
+    //console.log(event)
+    var _id = event.target.attributes[3].value;
+    //console.log(_id)
     var value = event.target.value;
+    //console.log(value)
     this.result_testreport[key][_id][1][0] = value;
   }
   onchangeInput_1(event) {
@@ -388,6 +379,8 @@ export class ImageScanComponent implements OnInit {
 //Send request and get Response to show result
   
   onSubmit() {
+    this.successAlert = false;
+    this.errorAlert = false;
     this.error = false;
     this.largeImage = false;
     this.zoom = false;
@@ -400,8 +393,8 @@ export class ImageScanComponent implements OnInit {
     this.upArrow = true;
     this.downArrow = true;
     this.count();
-    console.log("Before")
-    console.log(this.result_testreport,this.result_date,this.result_imp,this.result_others)
+    //console.log("Before")
+    //console.log(this.result_testreport,this.result_date,this.result_imp,this.result_others)
     this.service.postTestResponse().subscribe(response =>{
       if (response.code === "success") {
         this.uploadButton = true;
@@ -444,9 +437,9 @@ export class ImageScanComponent implements OnInit {
               this.result_others[this.result__keys[i]] = response.response[this.result__keys[i]]
             }
           }
-          console.log("After")
-          console.log(this.result_testreport,this.result_date,this.result_imp,this.result_others)
-          console.log(this.result_others)
+          //console.log("After")
+          //console.log(this.result_testreport,this.result_date,this.result_imp,this.result_others)
+          //console.log(this.result_others)
           var date = this.result_date[0]
           this.result_date[0] = date.split("-").reverse().join("-")
           this.isLoading = false;
@@ -523,9 +516,8 @@ export class ImageScanComponent implements OnInit {
 
   }
 //Send data to database
-  
+
   public onSubmitImage() {
-    this.successAlert = false;
     this.getResult = false;
     this.markForReview = false;
     for (var i = 0; i < this.result__keys.length; i++){
@@ -552,7 +544,6 @@ export class ImageScanComponent implements OnInit {
         'fields': this.result
       }
     };
-    this.errorAlert = false;
     this.closebutton.nativeElement.click();
     this.service.postResponseSaveasImage(params).subscribe(response => {
       this.success = response;
@@ -562,15 +553,23 @@ export class ImageScanComponent implements OnInit {
         this.failedCount = this.failedCount + 1;
         this.duplicate_browse =false;
         this.largeImage = true;
+        this.fileNames = [];
         this.base64textString = [];
         this.image_view = "data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==";
         this.zoomIcon = false
-        this.form.value.select = null;
+        //this.form.value.select = null;
         this.result = {};
         this.result_testreport={};
         this.result_imp=[];
-        this.result_date=null;
+        this.result_date=[];
         this.result_others = {};
+        this.error = false;
+        this.uploadFileAlert = false;
+        this.waitForResponse = false;
+        this.DropdownId = false;
+        this.verifyDocumentId = false;
+        this.invalidDocumentId = false;
+        this.unKnownError = false;
         this.result_date_show = false;
         this.result_imp_show = false;
         this.result_testreport_show = false;
@@ -579,11 +578,11 @@ export class ImageScanComponent implements OnInit {
         this.uploadButton = false;
         this.disabledupload = true;
         this.newDocumentInput = false;
-        this.waitForResponse = false;
         this.zoom = false;
         this.duplicate_browse = false;
         this.DocumentIdUploaded = "";
-        this.form.reset();
+        this.Select_document_disabled = false;
+        this.selectDropdownId = "";
       }
       else{
         this.errorAlert = true;
@@ -596,9 +595,6 @@ export class ImageScanComponent implements OnInit {
 
 //Send data to database
   public onSubmitText() {
-
-    this.successAlert = false;
-
     for (var i = 0; i < this.result__keys.length; i++){
       if (this.result__keys[i] === 'Test_Report') {
         this.result[this.result__keys[i]] = this.result_testreport
@@ -623,8 +619,6 @@ export class ImageScanComponent implements OnInit {
         'fields': this.result
       }
     };
-    this.errorAlert = false;
-    this.successAlert = false;
     this.closebutton1.nativeElement.click();
     this.service.postResponseSaveasText(finalOutput).subscribe(response => {
        this.success = response;
@@ -635,16 +629,24 @@ export class ImageScanComponent implements OnInit {
         this.tableShow = false;
         this.duplicate_browse =false;
         this.largeImage = true;
+        this.fileNames = [];
         this.base64textString = [];
         this.image_view = "data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==";
         this.zoomIcon = false
-        this.form.value.select = null;
+        //this.form.value.select = null;
         this.getResult = false;
         this.result = {};
         this.result_testreport={};
         this.result_imp=[];
-        this.result_date=null;
+        this.result_date=[];
         this.result_others = {};
+        this.error = false;
+        this.uploadFileAlert = false;
+        this.waitForResponse = false;
+        this.DropdownId = false;
+        this.verifyDocumentId = false;
+        this.invalidDocumentId = false;
+        this.unKnownError = false;
         this.result_date_show = false;
         this.result_imp_show = false;
         this.result_testreport_show = false;
@@ -652,10 +654,10 @@ export class ImageScanComponent implements OnInit {
         this.uploadButton = false;
         this.disabledupload = true;
         this.zoom = false;
-        this.waitForResponse = false;
         this.duplicate_browse = false;
         this.DocumentIdUploaded = "";
-        this.form.reset();
+        this.Select_document_disabled = false;
+        this.selectDropdownId = "";
       }
       else{
         this.errorAlert = true;
