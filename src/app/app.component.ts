69b7d4dfa458f97eed420b48dca92914928aadb9
NiThIn-S <48729197@users.noreply.github.com>
2021-06-11 14:28:34 +0530

    optimized code and added new icons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+// import { ConnectionService } from 'ngx-connection-service';
 
 @Component({
   selector: 'app-root',
@@ -12,7 +12,23 @@ export class AppComponent implements OnInit {
   ngOnInit(){
     
   }
-
+  // hasNetworkConnection: boolean;
+  // hasInternetAccess: boolean;
+  // status: string;
+ 
+  // constructor(private connectionService: ConnectionService) {
+  //   this.connectionService.monitor().subscribe(currentState => {
+  //     this.hasNetworkConnection = currentState.hasNetworkConnection;
+  //     this.hasInternetAccess = currentState.hasInternetAccess;
+  //     if (this.hasNetworkConnection && this.hasInternetAccess) {
+  //       this.status = 'ONLINE';
+        
+  //     } else {
+  //       this.status = 'OFFLINE';
+  //     }
+  //     console.log(this.status)
+  //   });
+  // }
   
 
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+// import {ConnectionServiceModule} from 'ngx-connection-service';
 
 import { AppComponent } from './app.component';
 import { ImageScanComponent } from './image-scan/image-scan.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { NgxBootstrapIconsModule, allIcons } from 'ngx-bootstrap-icons';
-import { CountdownModule } from 'ngx-countdown';
+// import { NgxBootstrapIconsModule, allIcons } from 'ngx-bootstrap-icons';
+// import { CountdownModule } from 'ngx-countdown';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import {MatDividerModule} from '@angular/material/divider'
+// import {MatDividerModule} from '@angular/material/divider'
 
 import {ZoomComponentComponent} from "./zoom-component/zoom-component.component"
 
@@ -25,9 +26,10 @@ import {ZoomComponentComponent} from "./zoom-component/zoom-component.component"
     FormsModule,
     ReactiveFormsModule,
     HttpClientModule,
-    CountdownModule,
-    MatDividerModule,
-    NgxBootstrapIconsModule.pick(allIcons),
+    // CountdownModule,
+    // MatDividerModule,
+    // NgxBootstrapIconsModule.pick(allIcons),
+    // ConnectionServiceModule
     ],
   providers: [],
   bootstrap: [AppComponent],
